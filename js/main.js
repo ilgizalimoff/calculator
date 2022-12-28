6dcb863f8e4234008bad61e2d97b6acc804fab05
ILGIZ <ilgizalimoff@yandex.ru>
2022-12-28 20:28:32 +0400

    ready app

--- js/main.js
+++ js/main.js
@@ -1,22 +1,72 @@
 let inputField = document.getElementById('inputNumberField')
-let stroka = ''
-let result = document.getElementById('result')
+let resultBtn = document.getElementById('result')
 let clear = document.getElementById('clear')
+let operations = ['+', '-', '*', '/']
+let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+let currentValue1 = ''
+let currentValue2 = ''
+let currentOperation = ''
+let result = 0
 
 function clickFunc(param) {
-    stroka += String(param)   
-    inputField.value = stroka 
-}   
+    operations.forEach(element => {
+        if (element == param) {
+            currentOperation = param
 
-result.addEventListener('click', resultFunc)
+            if (currentValue1 == '' && currentOperation == '-') {
+                currentValue1 += currentOperation
+                currentOperation = ''
+                inputField.value += currentValue1
+            }
+            else {
+                inputField.value = currentOperation
+                if (result == 0 && currentValue1 != '-') {
+                    currentValue2 = currentValue1
 
-function resultFunc(){
-    inputField.value = eval(inputField.value)
-    stroka = ''
+                }
+                else {
+                    currentValue2 = result
+                    currentValue1 = ''
+                }
+            }
+
+        }
+    })
+    numbers.forEach(element => {
+        if (element == param) {
+            if (currentValue1 == currentValue2) { currentValue1 = '' }
+            currentValue1 += param
+            inputField.value = currentValue1
+        }
+    })
 }
+
+
+function resultFunc() {
+    switch (currentOperation) {
+        case '+':
+            result = +currentValue2 + +currentValue1
+            break
+        case '-':
+            result = +currentValue2 - +currentValue1
+            break
+        case '/':
+            result = +currentValue2 / +currentValue1
+            break
+        case '*':
+            result = +currentValue2 * +currentValue1
+            break
+    }
+    inputField.value = result
+}
+resultBtn.addEventListener('click', resultFunc)
 
 clear.addEventListener('click', clearField)
 
-function clearField(){
-    stroka = inputField.value = ''
+function clearField() {
+    currentValue1 = ''
+    currentValue2 = ''
+    currentOperation = ''
+    inputField.value = ''
+    result = ''
 }
