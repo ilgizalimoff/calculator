a47c2e32bd2ea8437d359419debe9a17d861462a
ILGIZ <ilgizalimoff@yandex.ru>
2023-01-02 21:24:40 +0400

    operation fixed

--- js/main.js
+++ js/main.js
@@ -2,70 +2,73 @@ let inputField = document.getElementById('inputNumberField')
 let resultBtn = document.getElementById('result')
 let clear = document.getElementById('clear')
 let operations = ['+', '-', '*', '/']
-let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-let currentValue1 = ''
-let currentValue2 = ''
+let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
+let firstValue = ''
+let secondValue = ''
 let currentOperation = ''
 let result = 0
 
-function clickFunc(param) {
-    operations.forEach(element => {
-        if (element == param) {
-            currentOperation = param
-
-            if (currentValue1 == '' && currentOperation == '-') {
-                currentValue1 += currentOperation
-                currentOperation = ''
-                inputField.value += currentValue1
-            }
-            else {
-                inputField.value = currentOperation
-                if (result == 0 && currentValue1 != '-') {
-                    currentValue2 = currentValue1
-
-                }
-                else {
-                    currentValue2 = result
-                    currentValue1 = ''
-                }
-            }
-
-        }
-    })
+function numberClick(param) {
     numbers.forEach(element => {
         if (element == param) {
-            if (currentValue1 == currentValue2) { currentValue1 = '' }
-            currentValue1 += param
-            inputField.value = currentValue1
+            if (firstValue == secondValue) { firstValue = '' }
+            firstValue += param
+            inputField.value = firstValue
         }
     })
 }
 
+function operationClick(param) {
+    operations.forEach(element => {
+        if (element == param) {
+            currentOperation = param
+            if (firstValue == '' && currentOperation == '-') {
+                firstValue += currentOperation
+                currentOperation = ''
+                inputField.value += firstValue
+            }
+            else if (firstValue !== '') {
+                inputField.value = currentOperation
+                if (result == 0 && firstValue != '-') {
+                    secondValue = firstValue
+                }
+                else {
+                    secondValue = result
+                    firstValue = ''
+                }
+            }
+        }
+    })
+}
 
 function resultFunc() {
     switch (currentOperation) {
         case '+':
-            result = +currentValue2 + +currentValue1
+            result = (+secondValue + +firstValue)
             break
         case '-':
-            result = +currentValue2 - +currentValue1
+            result = +secondValue - +firstValue
             break
         case '/':
-            result = +currentValue2 / +currentValue1
+            result = +secondValue / +firstValue
             break
         case '*':
-            result = +currentValue2 * +currentValue1
+            result = +secondValue * +firstValue
+            break
+        case '':
+            result = +firstValue
             break
     }
     inputField.value = result
 }
+
 resultBtn.addEventListener('click', resultFunc)
 
 clear.addEventListener('click', clearField)
 
 function clearField() {
-    currentValue1 = ''
-    currentValue2 = ''
+    firstValue = ''
+    secondValue = ''
     currentOperation = ''
     inputField.value = ''
     result = ''
